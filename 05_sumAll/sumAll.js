eb8ee22d4959c4a5d2a79a6857fd0680461b12ba
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-05 21:17:35 +0200

    finalize 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+function nonValid(num){
+    if (num < 0 || typeof num!=='number') return true;
+    return false;
+}
 
+const sumAll = function(num1,num2) {
+    const lowerNumber = num1<num2? num1 : num2;
+    const largerNumber = lowerNumber === num1 ? num2 : num1;
+    let sum = 0;
+
+    if(nonValid(lowerNumber) || nonValid(largerNumber)) return 'ERROR';
+    for(let i=lowerNumber;i<=largerNumber;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
