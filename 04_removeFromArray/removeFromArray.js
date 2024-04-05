21595cafc709545fda18805f910e6e4166720884
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-05 21:03:06 +0200

    finalize 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...num) {
+    for(let i=0;i<num.length;i++){
+    array = array.filter(e=>e!==num[i]);
+}
 
+    return array;
 };
 
 // Do not edit below this line
