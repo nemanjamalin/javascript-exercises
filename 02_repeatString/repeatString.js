3a8357b1be6db92df489b67fe516ab826ea56123
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-05 20:35:03 +0200

    finalize 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    newString = '';
+    if(num < 0) return "ERROR";
+    for(let i = 1;i<=num;i++){
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
