21baa6ed8841cdfc1c4e5563964e06f57e061b77
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-13 16:49:38 +0200

    finalize 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = +number;
+    if(number ===0) return 0;
+    if(number < 0) return "OOPS";
+    if(number === 1 || number === 2) return 1;
+    let fibonacci = [1,1];
+    for(let i = 2;i<number;i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    return fibonacci[number-1];
 
 };
 
