618ec60757cc0243f4376d8c145b362735e00501
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-13 16:13:05 +0200

    finalize 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	if(numbers.length === 0) return 0;
+  return numbers.reduce((acc,number)=>acc+number,0); 
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  if(numbers.length === 0) return 0;
+  return numbers.reduce((acc,number)=>acc*number,1); 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0 || number === 1) return 1;
+  let factorial = 1;
+  for(let i=2;i<=number;i++){
+    factorial = factorial * i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
