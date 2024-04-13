e7c8ea0793c810a7e2aa1081c64787149bf95768
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-13 17:40:11 +0200

    finalize 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestAge = people.reduce((acc,person)=>{
+        let age = (person.yearOfDeath ? person.yearOfDeath : 2024) - person.yearOfBirth;
+        if(acc<age) acc = age;
+        return acc;
+    },-1);
 
+    const oldestPerson = people.filter((person) =>{
+        let age = (person.yearOfDeath ? person.yearOfDeath : 2024) - person.yearOfBirth;
+        return age === oldestAge;
+    })
+
+    return oldestPerson[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
