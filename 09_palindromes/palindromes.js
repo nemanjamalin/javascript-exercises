1ecb9ebb2a19638b5dda86c9d8b0503e276157c7
nemanjamalin <147262488+nemanjamalin@users.noreply.github.com>
2024-04-13 16:37:07 +0200

    finalize 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.trim();
+    string = string.replaceAll(' ','');
+    string = string.replaceAll('.','');
+    string = string.replaceAll(',','');
+    string = string.replaceAll('!','');
+    string = string.toUpperCase();
 
+
+    let newString = "";
+    for(let i=string.length -1;i>=0;i--){
+        newString+=string[i];
+    }
+    return newString == string ? true : false;
 };
 
 // Do not edit below this line
